3b8732eea57ddda7926b0eaa140c60dfe214ebf1
Graziano Vincini <graziano.vincini@caffeinalab.com>
2015-01-23 14:53:47 +0100

    1.3.2
    Signed-off-by: Graziano Vincini <graziano.vincini@caffeinalab.com>

--- aeria.php
+++ aeria.php
@@ -5,14 +5,14 @@
  * Author: Caffeina Srl
  * Author URI: http://caffeina.co
  * Plugin URI: https://github.com/CaffeinaLab/aeria
- * Version: 1.3.1
+ * Version: 1.3.2
  */
 
 // Exit if accessed directly
 if( false === defined('ABSPATH') ) exit;
 
 // The Framework version
-define('AERIA','1.3.1');
+define('AERIA','1.3.2');
 
 // Store whether or not we're in the admin
 if( false === defined('IS_ADMIN') ) define( 'IS_ADMIN',  is_admin() );
