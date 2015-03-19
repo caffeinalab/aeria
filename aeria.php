551c92345d3a5c02022b8f420ff511520a733faf
Stefano Azzolini <lastguest@gmail.com>
2015-03-19 15:53:24 +0100

    Update aeria.php

--- aeria.php
+++ aeria.php
@@ -5,14 +5,14 @@
  * Author: Caffeina Srl
  * Author URI: http://caffeina.co
  * Plugin URI: https://github.com/CaffeinaLab/aeria
- * Version: 1.3.7
+ * Version: 1.3.8
  */
 
 // Exit if accessed directly
 if( false === defined('ABSPATH') ) exit;
 
 // The Framework version
-define('AERIA','1.3.7');
+define('AERIA','1.3.8');
 
 // Store whether or not we're in the admin
 if( false === defined('IS_ADMIN') ) define( 'IS_ADMIN',  is_admin() );
