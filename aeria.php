e8ae20ad1b0056ce478ad3a7bd5c2fe8b7f0d2f6
Graziano Vincini <graziano.vincini@caffeinalab.com>
2015-11-02 14:53:35 +0100

    1.8.5

--- aeria.php
+++ aeria.php
@@ -5,14 +5,14 @@
  * Author: Caffeina
  * Author URI: http://caffeina.it
  * Plugin URI: https://github.com/CaffeinaLab/aeria
- * Version: 1.8.4
+ * Version: 1.8.5
  */
 
 // Exit if accessed directly
 if( false === defined('ABSPATH') ) exit;
 
 // The Framework version
-define('AERIA','1.8.4');
+define('AERIA','1.8.5');
 
 // Store whether or not we're in the admin
 if( false === defined('IS_ADMIN') ) define( 'IS_ADMIN',  is_admin() );
