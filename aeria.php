ea60cf6e9d3d700e9256283ef303fbef601b6345
Vito Famigletti <vito.famiglietti@caffeina.com>
2017-05-09 14:22:44 +0200

    1.8.82

--- aeria.php
+++ aeria.php
@@ -5,14 +5,14 @@
  * Author: Caffeina
  * Author URI: http://caffeina.com
  * Plugin URI: https://github.com/CaffeinaLab/aeria
- * Version: 1.8.81
+ * Version: 1.8.82
  */
 
 // Exit if accessed directly
 if( false === defined('ABSPATH') ) exit;
 
 // The Framework version
-define('AERIA','1.8.81');
+define('AERIA','1.8.82');
 
 // Store whether or not we're in the admin
 if( false === defined('IS_ADMIN') ) define( 'IS_ADMIN',  is_admin() );
