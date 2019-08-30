e0c012748a8c3fd82ae55351baa3d689eefd05f9
Alberto Parziale <alberto.parziale@caffeina.com>
2019-08-30 14:10:16 +0200

    bump to version 3.0.0

--- Aeria/Aeria.php
+++ Aeria/Aeria.php
@@ -25,7 +25,7 @@ use Aeria\RenderEngine\ServiceProviders\RenderEngineServiceProvider;
 
 class Aeria extends Container
 {
-    public const VERSION = '0.7.0';
+    public const VERSION = '3.0.0';
 
     public function __construct()
     {
