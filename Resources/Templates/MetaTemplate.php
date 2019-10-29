e2c4675601e6aef790ec3318cde652b7eeb35b32
Alberto Parziale <alberto.parziale@caffeina.com>
2019-10-29 12:06:02 +0100

    fix case sensitive path

--- Resources/Templates/MetaTemplate.php
+++
@@ -1,7 +0,0 @@
-<div id="aeriaApp-<?=$data['metabox']['id']?>" class="aeriaApp">
-  <?php wp_editor( '', 'loadTinymce'); ?>
-</div>
-<script>
-  window.aeriaMetaboxes = window.aeriaMetaboxes || [];
-  window.aeriaMetaboxes.push(<?=wp_json_encode($data['metabox']);?>);
-</script>
