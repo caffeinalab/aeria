88d2f1ffa5e668984fc3a485577b2fcdcbc6f6dd
Alberto Parziale <alberto.parziale@caffeina.com>
2019-11-15 16:33:54 +0000

    Enable multiple values in 'when' input condition and add possibility to hide the title section  - Version bump to 3.0.15

--- Aeria/Aeria.php
+++ Aeria/Aeria.php
@@ -33,7 +33,7 @@ use Aeria\RenderEngine\ServiceProviders\RenderEngineServiceProvider;
  */
 class Aeria extends Container
 {
-    public const VERSION = '3.0.14';
+    public const VERSION = '3.0.15';
     /**
      * Constructs the Aeria container
      * 
--- Aeria/Updater/Updater.php
+++ Aeria/Updater/Updater.php
@@ -1,54 +1,52 @@
 <?php
+
 namespace Aeria\Updater;
 
 /**
- * The Updater is in charge of checking updates on the GitHub repo
- * 
+ * The Updater is in charge of checking updates on the GitHub repo.
+ *
  * @category Updater
- * @package  Aeria
+ *
  * @author   Alberto Parziale <alberto.parziale@caffeina.com>
  * @license  https://github.com/caffeinalab/aeria/blob/master/LICENSE  MIT license
- * @link     https://github.com/caffeinalab/aeria
+ *
+ * @see     https://github.com/caffeinalab/aeria
  */
 class Updater
 {
     /**
-     * Constructs the Updater object
+     * Constructs the Updater object.
      *
-     * @return void
      *
-     * @access public
      * @since  Method available since Release 3.0.0
      */
     public function __construct()
     {
         // define the alternative API for updating checking
-        add_filter("pre_set_site_transient_update_plugins", array($this, "checkVersion"));
+        add_filter('pre_set_site_transient_update_plugins', array($this, 'checkVersion'));
         // Define the alternative response for information checking
-        add_filter("plugins_api", array($this, "setPluginInfo"), 10, 3);
+        add_filter('plugins_api', array($this, 'setPluginInfo'), 10, 3);
         // reactivate plugin
-        add_filter("upgrader_post_install", array($this, "postInstall"), 10, 3);
+        add_filter('upgrader_post_install', array($this, 'postInstall'), 10, 3);
     }
+
     /**
-     * Saves the provided config
+     * Saves the provided config.
      *
      * @param array $config the configuration
-     * 
-     * @return void
      *
-     * @access public
      * @since  Method available since Release 3.0.0
      */
     public function config($config)
     {
         $this->config = $config;
     }
+
     /**
-     * Gets the plugin data from plugin.php
+     * Gets the plugin data from plugin.php.
      *
      * @return mixed the plugin data
      *
-     * @access private
      * @since  Method available since Release 3.0.0
      */
     private function getPluginData()
@@ -59,22 +57,22 @@ class Updater
 
         include_once ABSPATH.'/wp-admin/includes/plugin.php';
 
-        $this->pluginData = get_plugin_data(WP_PLUGIN_DIR.'/'.$this->config["slug"]);
+        $this->pluginData = get_plugin_data(WP_PLUGIN_DIR.'/'.$this->config['slug']);
 
-        $githubUrl = parse_url($this->pluginData["PluginURI"]);
+        $githubUrl = parse_url($this->pluginData['PluginURI']);
         $githubInfo = explode('/', $githubUrl['path']);
 
         $this->github = [
-          "user" => $githubInfo[1],
-          "repository" => $githubInfo[2]
+          'user' => $githubInfo[1],
+          'repository' => $githubInfo[2],
         ];
     }
+
     /**
-     * Gets the release infos from the GitHub repo
+     * Gets the release infos from the GitHub repo.
      *
      * @return mixed|void a transient or nothing
      *
-     * @access private
      * @since  Method available since Release 3.0.0
      */
     private function getRepoReleaseInfo()
@@ -84,15 +82,17 @@ class Updater
             return;
         }
 
-        $transient = get_transient("{$this->github["user"]}_{$this->github["repository"]}_transient_update");
+        $transient = get_transient("{$this->github['user']}_{$this->github['repository']}_transient_update");
         if ($transient !== false) {
-            return $transient;
+            $this->githubAPIResult = $transient;
+
+            return;
         }
         // Query the GitHub API
-        $url = "https://api.github.com/repos/{$this->github["user"]}/{$this->github["repository"]}/releases";
+        $url = "https://api.github.com/repos/{$this->github['user']}/{$this->github['repository']}/releases";
         // We need the access token for private repos
-        if (!empty($this->config["access_token"])) {
-            $url = add_query_arg(array("access_token" => $this->config["access_token"]), $url);
+        if (!empty($this->config['access_token'])) {
+            $url = add_query_arg(array('access_token' => $this->config['access_token']), $url);
         }
 
         // Get the results
@@ -104,16 +104,16 @@ class Updater
         if (is_array($this->githubAPIResult)) {
             $this->githubAPIResult = $this->githubAPIResult[0];
         }
-        set_transient("{$this->github["user"]}_{$this->github["repository"]}_transient_update", $this->githubAPIResult, 3.600);
+        set_transient("{$this->github['user']}_{$this->github['repository']}_transient_update", $this->githubAPIResult, 3.600);
     }
+
     /**
-     * Compares the current version to the installed one
+     * Compares the current version to the installed one.
      *
      * @param mixed $transient the updates transient
-     * 
+     *
      * @return mixed the transient
      *
-     * @access public
      * @since  Method available since Release 3.0.0
      */
     public function checkVersion($transient)
@@ -134,32 +134,32 @@ class Updater
         if ($doUpdate == 1) {
             $package = $this->githubAPIResult->zipball_url;
             // Include the access token for private GitHub repos
-            if (!empty($this->config["access_token"])) {
-                $package = add_query_arg(array( "access_token" => $this->config["access_token"]), $package);
+            if (!empty($this->config['access_token'])) {
+                $package = add_query_arg(array('access_token' => $this->config['access_token']), $package);
             }
 
             $obj = new \StdClass();
-            $obj->slug = $this->config["slug"];
+            $obj->slug = $this->config['slug'];
             $obj->new_version = $this->githubAPIResult->tag_name;
-            $obj->url = $this->pluginData["PluginURI"];
+            $obj->url = $this->pluginData['PluginURI'];
             $obj->package = $package;
-            $obj->plugin = $this->config["slug"];
+            $obj->plugin = $this->config['slug'];
             $obj->icons = ['1x' => '/wp-content/plugins/aeria/Aeria/aeria-transparent.png'];
-            $transient->response[$this->config["slug"]] = $obj;
+            $transient->response[$this->config['slug']] = $obj;
         }
+
         return $transient;
     }
 
     /**
-     * Sets the plugin infos
-     * 
+     * Sets the plugin infos.
+     *
      * @param mixed $false    unused parameter sent by the filter
      * @param mixed $action   unused parameter sent by the filter
      * @param mixed $response the updates check response
-     * 
+     *
      * @return mixed the response for the transient
      *
-     * @access public
      * @since  Method available since Release 3.0.0
      */
     public function setPluginInfo($false, $action, $response)
@@ -169,25 +169,25 @@ class Updater
         $this->getRepoReleaseInfo();
 
         // If nothing is found, do nothing
-        if (empty($response->slug) || $response->slug != $this->config["slug"]) {
+        if (empty($response->slug) || $response->slug != $this->config['slug']) {
             return false;
         }
         // Add our plugin information
         $response->last_updated = $this->githubAPIResult->published_at;
-        $response->slug = $this->config["slug"];
-        $response->name = $this->pluginData["Name"];
-        $response->plugin_name  = $this->pluginData["Name"];
+        $response->slug = $this->config['slug'];
+        $response->name = $this->pluginData['Name'];
+        $response->plugin_name = $this->pluginData['Name'];
         $response->version = $this->githubAPIResult->tag_name;
-        $response->author = $this->pluginData["AuthorName"];
-        $response->homepage = $this->pluginData["PluginURI"];
+        $response->author = $this->pluginData['AuthorName'];
+        $response->homepage = $this->pluginData['PluginURI'];
 
-        $response->sections = array( 'description' =>$this->githubAPIResult->body );
+        $response->sections = array('description' => $this->githubAPIResult->body);
         // This is our release download zip file
         $downloadLink = $this->githubAPIResult->zipball_url;
         // Include the access token for private GitHub repos
-        if ( !empty( $this->config["access_token"] ) ) {
+        if (!empty($this->config['access_token'])) {
             $downloadLink = add_query_arg(
-                array( "access_token" => $this->config["access_token"] ),
+                array('access_token' => $this->config['access_token']),
                 $downloadLink
             );
         }
@@ -197,15 +197,14 @@ class Updater
     }
 
     /**
-     * Reactivates the plugin after installation
-     * 
+     * Reactivates the plugin after installation.
+     *
      * @param mixed $true       unused parameter sent by the filter
      * @param mixed $hook_extra unused parameter sent by the filter
      * @param mixed $result     the result of the update
-     * 
+     *
      * @return mixed the response for the transient
      *
-     * @access public
      * @since  Method available since Release 3.0.0
      */
     public function postInstall($true, $hook_extra, $result)
@@ -217,9 +216,10 @@ class Updater
         $result['destination'] = $proper_destination;
         $activate = activate_plugin(WP_PLUGIN_DIR.'/'.$this->config['slug']);
         // Output the update message
-        $fail  = __('The plugin has been updated, but could not be reactivated. Please reactivate it manually.', 'github_plugin_updater');
+        $fail = __('The plugin has been updated, but could not be reactivated. Please reactivate it manually.', 'github_plugin_updater');
         $success = __('Plugin reactivated successfully.', 'github_plugin_updater');
         echo is_wp_error($activate) ? $fail : $success;
+
         return $result;
     }
 }
