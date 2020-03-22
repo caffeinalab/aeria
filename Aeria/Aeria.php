6dca3469854db773e02e0f4fd67a30e7245c6d26
Alberto Parziale <alberto.parziale@caffeina.com>
2020-03-22 16:47:00 +0100

    update Sortable components

--- Aeria/Aeria.php
+++ Aeria/Aeria.php
@@ -31,7 +31,7 @@ use Aeria\RenderEngine\ServiceProviders\RenderEngineServiceProvider;
  */
 class Aeria extends Container
 {
-    const VERSION = '3.1.4';
+    const VERSION = '3.1.5';
 
     /**
      * Constructs the Aeria container.
