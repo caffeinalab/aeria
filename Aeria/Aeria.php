786006b8b8caf0e0aa4662bd80678affd3548dec
Elisa Losito <elisa.losito@caffeina.com>
2020-11-06 14:19:54 +0100

    update version

--- Aeria/Aeria.php
+++ Aeria/Aeria.php
@@ -31,7 +31,7 @@ use Aeria\RenderEngine\ServiceProviders\RenderEngineServiceProvider;
  */
 class Aeria extends Container
 {
-    const VERSION = '3.2.10';
+    const VERSION = '3.2.14';
 
     /**
      * Constructs the Aeria container.
